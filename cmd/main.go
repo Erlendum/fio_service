e9afc4251933a0678006c83e5099f866d88cecb8
Erlendum <erlendum.ds@gmail.com>
2023-09-14 17:51:18 +0300

    server was added.

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fio_service/internal/config"
+	"fio_service/internal/server"
 	cache "fio_service/pkg/cache/redis"
 	"fio_service/pkg/database"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
 func main() {
@@ -25,4 +30,26 @@ func main() {
 	}
 	fmt.Println(db)
 	fmt.Println(memCache)
+
+	srv := server.NewServer(cfg, nil)
+	go func() {
+		if err := srv.Run(); !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("error occurred while running http server: %s\n", err.Error())
+		}
+	}()
+
+	log.Println("server started ", cfg.Server.Port)
+
+	quit := make(chan os.Signal, 1)
+
+	<-quit
+
+	const timeout = 5 * time.Second
+
+	ctx, shutdown := context.WithTimeout(context.Background(), timeout)
+	defer shutdown()
+
+	if err := srv.Stop(ctx); err != nil {
+		log.Fatalf("failed to stop server %v", err)
+	}
 }
